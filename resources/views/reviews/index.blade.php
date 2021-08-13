c2dbc4a56fa795c693d1fd6075679521ab1f5c9a
kia4545 <81552960+kia4545@users.noreply.github.com>
2021-08-13 17:45:39 +0900

    レビュー検索画面の作成

--- resources/views/reviews/index.blade.php
+++ resources/views/reviews/index.blade.php
@@ -9,25 +9,6 @@
     <div class="row justify-content-center">
         <div class="col-md-10">
             <div class="row">
-                <p class="col-md-4 text-md-right category">カテゴリ</p>
-                <div class="col-md-4 category">
-                    <select name="category_id" class="custom-select form-control @error('category_id') is-invalid @enderror">
-                        @foreach ($conditions as $condition)
-                        <option value="{{$condition->id}}" {{old('category_id')==$condition->id ? 'selected' : ''}}>
-                            {{$condition->name}}
-                        </option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-            <div class="row freeword">
-                <p class="col-md-4 text-md-right freeword">フリーワード</p>
-                <div class="col-md-6">
-                    <input type="text" name="word" value="" class="freeword">
-                </div>
-                <button type="submit" class="btn btn-block btn-primary col-md-4 mx-auto py-2 mt-3 search">検索する</button>
-            </div>
-            <div class="row">
                 @foreach ($reviews as $review)
                 <div class="col-md-6">
                     <div class="card mb-5">
